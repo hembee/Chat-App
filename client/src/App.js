9a1b72e5d791dd2038ae1f3b1b0928dffd3a7754
Asagba Mubarak Anthony <hembee999@gmail.com>
2022-10-11 16:06:54 +0100

    Restored public folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>
+        Hello new world, All the boys and girls, I got some true stories to tell
+        , you're back outside but it still hurts woooooaaaaahhhhh
+      </h1>
+    </div>
+  );
 }
 
 export default App;
