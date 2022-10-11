4a35b95cc6d24a341458322dd73cc90d7ba9193e
Asagba Mubarak Anthony <hembee999@gmail.com>
2022-10-11 17:17:17 +0100

    progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
 import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chats from "./components/Chats";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = () => {
+    if (username !== "" && password !== "") {
+      socket.emit("join_room", password);
+    }
+    setUsername("");
+    setPassword("");
+  };
+
   return (
     <div className="App">
-      <h1>
-        Hello new world, All the boys and girls, I got some true stories to tell
-        , you're back outside but it still hurts woooooaaaaahhhhh
-      </h1>
+      <h3>Join A Chat</h3>
+      <input
+        type="text"
+        placeholder="Wunmi ❤"
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+        value={username}
+      />
+      <input
+        type="text"
+        placeholder="Room ID"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
+      />
+      <button onClick={submitHandler}>Join A Room</button>
+      <Chats socket={socket} username={username} password={password} />
     </div>
   );
 }
---
+++ client/src/components/Chats.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Chats({socket, username, password}) {
+  return (
+    <div>
+      <div className="chat-header"></div>
+      <div className="chat-body"></div>
+      <div className="chat-footer"></div>
+    </div>
+  )
+}
+
+export default Chats
