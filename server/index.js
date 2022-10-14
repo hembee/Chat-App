241e15a73e6a94fe10b2bc0abde4c9a397b2dc94
Asagba Mubarak Anthony <hembee999@gmail.com>
2022-10-14 08:46:10 +0100

    gh pages

--- server/index.js
+++ server/index.js
@@ -19,12 +19,13 @@ io.on("connection", (socket) => {
   console.log(`User connected ${socket.id}`);
 
   socket.on("join_room", (data) => {
-      socket.join(data);
-      console.log(`User with ID:${socket.id} joined room ${data}`)
+    socket.join(data);
+    console.log(`User with ID:${socket.id} joined room ${data}`);
   });
-  socket.on("send_message", (data)=>{
-    socket.to(data.password).emit("receive_message", data)
-  })
+
+  socket.on("send_message", (data) => {
+    socket.to(data.password).emit("receive_message", data);
+  });
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
