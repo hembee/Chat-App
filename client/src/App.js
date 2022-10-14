463e85ad2806d11ebc97537e7aca771a31ea1d13
hembee <hembee999@gmail.com>
2022-10-14 10:44:45 +0100

    Cors

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chats from "./components/Chats";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://hembee.github.io/Chat-App");
 
 function App() {
   const [username, setUsername] = useState("");
